fe654c6dc830569666db50121ca6c983974d33ff
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-22 11:06:02 +0800

    initial: initial structure

---
+++ apps/discord-bot/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["src/**/*.ts", "!src/**/*.d.ts", "src/**/*.tsx", "!src/**/*.test.ts*"],
+    clean: true,
+    skipNodeModulesBundle: true,
+    tsconfig: "tsconfig.json",
+    noExternal: ["@ixveria/environment"],
+});
