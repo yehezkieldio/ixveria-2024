9209860ecbd6b270ca5a42b7404d20ecc3dc3e50
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-23 04:53:56 +0800

    chore: tsup already has the option for cleaning build dir

--- apps/discord-bot/tsup.config.ts
+++ apps/discord-bot/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     entry: ["src/**/*.ts", "!src/**/*.d.ts", "src/**/*.tsx", "!src/**/*.test.ts*"],
     clean: true,
     sourcemap: true,
+    minify: true,
     skipNodeModulesBundle: true,
     tsconfig: "tsconfig.json",
     noExternal: ["@ixveria/environment", "@ixveria/database"],
--- scripts/rimraf.ts
+++
@@ -1,4 +0,0 @@
-import { $ } from "bun";
-
-const path = process.argv[2] || process.cwd();
-await $`rm -rf ${path}/dist`.quiet();
